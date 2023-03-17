682e63091d23a268d8ba46744aa0ada91e973859
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-03-17 14:28:40 +0000

    all tests pass for Strings.scala

--- src/main/scala/utils/Strings.scala
+++ src/main/scala/utils/Strings.scala
@@ -1,16 +1,23 @@
 package utils
+import scala.collection.mutable.ArrayBuffer
 
 object Strings {
 
 // Function to decide and return the string key the note will be added to
-  def findString(note: Int): Unit = {
-    // 1. create list
-    val stringsWithNotePresent = List()
+  def findString(note: Int): Int = {
+    val stringsWithNotePresent = ArrayBuffer[Int]()
 
-    println(utils.StringsAndNotes.stringMap(0))
+    val guitarStrings = utils.Notes.guitarStrings
+    for ((stringIndex, stringNotes) <- utils.Notes.guitarStrings) {
+      if (stringNotes.contains(note)) {
+        stringsWithNotePresent += stringIndex
+      }
+    }
+    return stringsWithNotePresent.min
+// utils.Notes.guitarStrings.keys.head
     // 2. loop: if note found in each string array? create new list and push note to tail
     // 3. return the first element in the array (i.e the higher string)
-
+    // return stringsWithNotePresent.keys.head
   }
 
 }
--- src/test/scala/utils/StringsSpec.scala
+++ src/test/scala/utils/StringsSpec.scala
@@ -5,9 +5,21 @@ import org.scalatest.wordspec.AnyWordSpec
 class StringTest extends AnyWordSpec {
 
   "Strings" when {
-    "given 4" should {
-      "return 5" in {
+    "given a note" should {
+      "return 5 when given 4" in {
         assert(utils.Strings.findString(4) == 5)
+      }
+      "return 4 when given 5" in {
+        assert(utils.Strings.findString(5) == 4)
+      }
+      "return 4 when given 6" in {
+        assert(utils.Strings.findString(6) == 4)
+      }
+      "return 3 when given 10" in {
+        assert(utils.Strings.findString(10) == 3)
+      }
+      "return 3 when given 11" in {
+        assert(utils.Strings.findString(11) == 3)
       }
     }
   }
